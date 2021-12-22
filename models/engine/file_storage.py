65573cd98f02ab13867797cdd5f0d385f25a66d8
Faitholo <ikhidero.faithy@gmail.com>
2021-12-22 13:08:21 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,6 +24,12 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+            
+    def delete(self, obj=None):
+        if obj is None:
+            return
+
+        FileStorage.__objects.pop(obj.id, 0)
 
     def reload(self):
         """Loads storage dictionary from file"""
