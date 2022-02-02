3f7d8dc6d3d208c45923f1f91e26a22188daa69e
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-02-02 20:57:51 +0100

    web_flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,19 +6,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ displays a HTML page with a list of states """
+    states = storage.all(State)
+    return render_template('7-states_list.html')
+
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ displays a HTML page with a list of states """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    return render_template('7-states_list.html', states=states)
-
 
 if __name__ == "__main__":
     """ Main Function """
