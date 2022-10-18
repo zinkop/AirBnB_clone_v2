238e8b7ef6f223897be2dfbe670388bf738043f6
zinkop <99264930+zinkop@users.noreply.github.com>
2022-10-18 19:44:41 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-""" A script that starts a flask web application"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ displays a HTML page with a list of states """
-    states = storage.all(State)
-    return render_template('7-states_list.html')
+def list_states():
+    '''
+    Lists states from db
+    '''
+    state_dict = storage.all('State')
+    state_list = []
+
+    for state in state_dict.values():
+        state_list.append(state)
+    return render_template('7-states_list.html', state_list=state_list)
+
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def teardown_app(e):
+    '''
+    teardown app context
+    '''
     storage.close()
 
-if __name__ == "__main__":
-    """ Main Function """
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
